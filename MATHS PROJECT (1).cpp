90bd28c38688a26ea045b83400e2a1c5cb9cfcf4
Rishabh Chauhan <95219453+Rishhsome@users.noreply.github.com>
2022-05-29 15:59:11 +0530

    Update MATHS PROJECT (1).cpp

--- MATHS PROJECT (1).cpp
+++ MATHS PROJECT (1).cpp
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <math.h>
 
-	float Funct(float x)    
+	float Funct(float x) 
 	{
-		return cos(x);     // Any function can be given and to be entered here!
+	        return cos(x);       // any function can be given and to be entered here!  
 	}
-	
-	void intermediate(float a,float b)
+
+        void intermediate(float a,float b)
 	{
-			
-		
-		if(Funct(a)*Funct(b)>=0)    // Recursion of Funct().
+		if(Funct(a)*Funct(b) >= 0)    
 		{
 			printf("\nYou have not assumed right a and b");
 			return;
@@ -18,36 +16,32 @@
 		
 		float c;
 		
-		while((b-a)>=0.0001)
+		while((b-a) >= 0.0001)
 		{
-			c=(a+b)/2;   // To find the middle point.
+			c = (a+b) / 2;   
 			
-			if(Funct(c)==0.00)    // Check if middle point is root.
+			if(Funct(c) == 0.0000)  
 			break;
-			
-			else if(Funct(c)*Funct(a)<0)     //Decide the side to repeat the step.
-			b=c;  
-			
+			else if(Funct(c) * Funct(a) < 0)    
+			b = c;  
 			else
-			a=c;  
+			a = c;  
 		}
-		
-		
 		printf("\nThe value of root is = %.4f",c);
 		float ans;
-		ans=Funct(c);
-		printf("\nThe value of f(c)=%.4f",ans);
+		ans = Funct(c);
+		printf("\nThe value of f(c) = %.4f",ans);
 	}
 	
 	int main()
 	{
 		float a,b;
-		printf("Enter the value of a=");
+		printf("Enter the value of a = ");
 		scanf("%f",&a);
-		printf("\nEnter the value of b=");
+		printf("\nEnter the value of b = ");
 		scanf("%f",&b);
-		
-		intermediate(a,b);   // Calling the function intermediate().
+		intermediate(a,b);  
+		return 0;
 	}
 	
 
